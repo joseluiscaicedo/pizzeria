54134ea9ac61616cf86e162f9a6f65a831fd57d3
jose caicedo <joseluiscl@gmail.com>
2021-08-01 18:37:09 -0500

    initial config

---
+++ src/constants.js
@@ -0,0 +1,12 @@
+export const pizzaImg = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305508/pizza/Pizza_et6arv.png';
+export const loginBestPizzaImg = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305499/pizza/Login-Best-Pizza_ra3mbr.png';
+export const userIcon = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305499/pizza/ic_usuario_ivhnj8.png';
+export const passwordIcon = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305504/pizza/ic_contrasena_hgy3xy.png';
+
+export const images = {
+  'Pizzeria Capitan America': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
+  'Pizzeria Iron Man': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
+  'Pizzeria Thor': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
+  'Pizzeria Black Widow': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
+  'Pizzeria Spider Man': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
+};
