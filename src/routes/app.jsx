9ebd84b2f588120a11d298dbee4500157a87b35a
jose caicedo <joseluiscl@gmail.com>
2021-08-02 11:23:48 -0500

    add routes

--- src/routes/app.jsx
+++ src/routes/app.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from '../components/Login';
+import NotFound from '../components/NotFound';
+import Details from '../components/Details';
 
 const App = () => {
 
   return (
-    <Login />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        <Route exact path='/Detalles' component={Details} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 };
 
