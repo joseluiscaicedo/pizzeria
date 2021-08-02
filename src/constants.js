6fed0856a32db47fa71983ef6a11f9192a048fb1
jose caicedo <joseluiscl@gmail.com>
2021-08-02 11:18:38 -0500

    add images from CDN

--- src/constants.js
+++ src/constants.js
@@ -2,11 +2,17 @@ export const pizzaImg = 'https://res.cloudinary.com/josecaicedo/image/upload/v16
 export const loginBestPizzaImg = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305499/pizza/Login-Best-Pizza_ra3mbr.png';
 export const userIcon = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305499/pizza/ic_usuario_ivhnj8.png';
 export const passwordIcon = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305504/pizza/ic_contrasena_hgy3xy.png';
+export const logoWhitePizza = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305499/pizza/Logo_jwwxrb.png';
+export const logoAllWhitePizza = 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305504/pizza/Best_Pizza_svio9m.png';
+export const facebookLogo = 'https://res.cloudinary.com/josecaicedo/image/upload/v1627882677/pizza/facebookWhiteLogo_jisua6.png';
+export const instagramLogo = 'https://res.cloudinary.com/josecaicedo/image/upload/v1627882677/pizza/instagramWhiteLogo_dsj2ev.png';
+export const delorean = 'https://res.cloudinary.com/josecaicedo/image/upload/v1627918882/pizza/Delorean_wpgma8.webp';
+export const backTotheHome = 'https://res.cloudinary.com/josecaicedo/image/upload/v1627918913/pizza/backToTheHomepage_ynrvyk.png';
 
 export const images = {
   'Pizzeria Capitan America': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
-  'Pizzeria Iron Man': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
-  'Pizzeria Thor': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
-  'Pizzeria Black Widow': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
-  'Pizzeria Spider Man': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/voglia_di_pizza_zwqujf.png',
+  'Pizzeria Iron Man': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305503/pizza/trulli_zjxtxx.png',
+  'Pizzeria Thor': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305501/pizza/pizzeria_camion_uyhywb.png',
+  'Pizzeria Black Widow': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305501/pizza/Sbarro_mjsalz.png',
+  'Pizzeria Spider Man': 'https://res.cloudinary.com/josecaicedo/image/upload/v1626305500/pizza/stroller_pizza_qdg85n.png',
 };
