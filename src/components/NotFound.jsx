ebf13bc176cfe026819ad8fe36f3a043536bbd92
jose caicedo <joseluiscl@gmail.com>
2021-08-02 11:26:52 -0500

    add NotFound Component

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {delorean, backTotheHome} from '../constants';
+
+import '../assets/styles/NotFound.scss';
+const NotFound = () => (
+  <body>
+    <section className='notFound'>
+      <div className='notFound__imgContainer'>
+        <img className='imgContainer__img' src={backTotheHome} alt='Back to the Homepage' />
+        <img className='imgContainer__img' src={delorean} alt='El Delorean, El Doc y Marti McFly' />
+      </div>
+      <div className='notFound__container'>
+        <h1 className='container__h1'>404</h1>
+        <h2 className='container__h2'>PAGE NOT FOUND</h2>
+        <h3 className='container__h3'>BACK TO HOME?</h3>
+        <a aria-describedby='GoToHome' href='/' className='container__anchor'>YES</a>
+        <a aria-describedby='GoOut' className='container__anchor' href='https://www.youtube.com/watch?v=G3AfIvJBcGo'>NO</a>
+      </div>
+    </section>
+  </body>
+);
+
+export default NotFound;
