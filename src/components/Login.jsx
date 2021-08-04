1f0f84bcf8e5e31d1f40e92dfc04a39791bbdce7
jose caicedo <joseluiscl@gmail.com>
2021-08-04 16:31:36 -0500

    add customHook useFetchData Login Component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Restaurants from './Restaurants';
 import LoginForm from './LoginForm';
-
+import useFetchData from '../hooks/useFetchData';
 const handleSubmit = ({ setAuthenticate, data, values }) => (event) => {
   event.preventDefault();
   data.users.forEach((element) => {
@@ -18,20 +18,13 @@ const handleInput = ({ setValues, values }) => (event) => {
   });
 };
 
+
 const Login = () => {
+  const APIURL = process.env.API_URL;
+  const data = useFetchData(APIURL);
+
   const [authenticate, setAuthenticate] = useState(false);
   const [values, setValues] = useState([]);
-  const [data, setData] = useState([]);
-  const APIURL = process.env.API_URL;
-
-  useEffect(() => {
-    fetch(APIURL)
-      .then((response) => response.json())
-      .then((info) => {
-        setData(info.response);
-      })
-      .catch((error) => console.log('error', error));
-  }, []);
 
   return (
     (!authenticate) ? (
